51f8d99f2080382eb82510ee822933a551f6e04c
xlmnope <lmgiroud@gmail.com>
2019-03-21 19:40:09 -0700

    updated schema w db name and updated apiRoutes to create new traveler

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,33 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all travelers
+  app.get("/api/voyager", function(req, res) {
+    db.Travel.findAll({}).then(function(dbVoyager) {
+      res.json(dbVoyager);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Create new traveler
+  app.post("/api/voyager", function(req, res) {
+    db.Travel.create({
+      name: req.body.name,
+      location: req.body.location,
+      language: req.body.language,
+      travelguide: req.body.travelguide
+    }).then(function(dbVoyager) {
+      res.json(dbVoyager);
+    
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
-  });
+  // Delete by id
+  // commenting this out for now because i dont think we will need to delete any travelers
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Travel.destroy({ where: { id: req.params.id } }).then(function(
+  //     dbExample
+  //   ) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
