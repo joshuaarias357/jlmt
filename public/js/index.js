94a86906074106fc4abac6f70e2698f35f2c9b9a
Josh <josh.arias714@gmail.com>
2019-03-28 21:05:16 -0700

    Fixed linter problems for push again

--- public/js/index.js
+++ public/js/index.js
@@ -110,20 +110,18 @@ var handleFormSubmit = function(event) {
   });
 };
 
-
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-    // var handleDeleteBtnClick = function() {
-    //   var idToDelete = $(this)
-    //     .parent()
-    //     .attr("data-id");
+// var handleDeleteBtnClick = function() {
+//   var idToDelete = $(this)
+//     .parent()
+//     .attr("data-id");
 
-    //   API.deleteExample(idToDelete).then(function() {
-    //     refreshExamples();
-    //   });
-    // };
+//   API.deleteExample(idToDelete).then(function() {
+//     refreshExamples();
+//   });
+// };
 
 // Add event listeners to the submit and delete buttons
-$("#submit").on("click", function() { handleFormSubmit(); printSimilarTravelers() });
+$("#submit").on("click", handleFormSubmit);
 //$exampleList.on("click", ".delete", handleDeleteBtnClick);
-
